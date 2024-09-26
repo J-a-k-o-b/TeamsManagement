c980ca40d477e4a56a88f994e3b9d30fbaf66ac3
Jakob Schaefer <jakob.schaefer@thinformatics.com>
2024-09-26 11:23:26 +0200

    workflow

--- generate_data.Tests.ps1
+++ generate_data.Tests.ps1
@@ -2,4 +2,9 @@ BeforeAll {
     #Lade...
     ."$PSScriptRoot\generate_data.ps1"
 }
-Describe "Generate-DummyData"
+Describe "Generate-DummyData"{
+    It "name des tests" {
+        $result=Generate-DummyData
+        $result.count | should -be [int] 
+    }
+}    
